e9912134699744a601bc89570daffb6a3a605587
John Cris <johncrismanabo9@gmail.com>
2022-07-08 14:23:24 +0800

    change to local script

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -212,10 +212,10 @@
   <script src="<?php echo base_url(); ?>assets/vendor/datatable/js/dataTables.bootstrap5.min.js"></script>
   <script src="<?php echo base_url(); ?>assets/vendor/datatable/js/dataTables.buttons.min.js"> </script>
   <script src="<?php echo base_url(); ?>assets/vendor/datatable/js/jszip.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/datatable/js/pdfmake.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/vendor/datatable/js/vfs_fonts.js"></script>
   <script src="<?php echo base_url(); ?>assets/vendor/datatable/js/buttons.print.min.js"></script>
-  <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script> 
+  <script src="<?php echo base_url(); ?>assets/vendor/datatable/js/buttons.html5.min.js"></script> 
   <script src="<?php echo base_url(); ?>assets/vendor/datatable/js/buttons.colVis.min.js"></script>
   <script src="<?php echo base_url() ?>assets/javascript/sweetalert.js"></script>
   <script src="<?php echo base_url() ?>assets/javascript/business_permit.js"></script>  
